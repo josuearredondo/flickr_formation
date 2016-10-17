a137bd347a0a1575101d952e0dac63dead595b28
josuearredondo <josue.arredondo@hotmail.fr>
2016-10-17 12:54:24 +0200

    Created custom Base Adapter, and UI elements

---
+++ app/src/main/java/com/josue/flickr/AdapterFlickr.java
@@ -0,0 +1,54 @@
+package com.josue.flickr;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdapterFlickr extends BaseAdapter {
+    private List<String> listFlickr;
+    private Context context;
+
+    public AdapterFlickr(Context context) {
+        listFlickr = new ArrayList<>();
+        this.context = context;
+    }
+    @Override
+    public int getCount() {
+        // TODO Auto-generated method stub
+        return listFlickr.size();
+    }
+    @Override
+    public Object getItem(int arg0) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    @Override
+    public long getItemId(int position) {
+        // TODO Auto-generated method stub
+        return position;
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.raw_layout, parent, false);
+        }
+        TextView textView = (TextView) convertView.findViewById(R.id.textRow);
+        textView.setText(listFlickr.get(position));
+        return convertView;
+    }
+
+    public void setListText(List listText) {
+        this.listFlickr = listText;
+        notifyDataSetChanged();
+    }
+}
+
+
--- app/src/main/java/com/josue/flickr/MainActivity.java
+++ app/src/main/java/com/josue/flickr/MainActivity.java
@@ -2,12 +2,47 @@ package com.josue.flickr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    Button bntChange;
 
+    AdapterFlickr adapter;
+    List<String> list1;
+    List<String> list2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.listView);
+        bntChange = (Button) findViewById(R.id.buttonChange);
+
+        list1 = new ArrayList<>();
+        list1.add("Toto");
+        list1.add("Titi");
+        list1.add("Tata");
+        adapter = new AdapterFlickr (this);
+        listView.setAdapter(adapter);
+        adapter.setListText(list1);
+        addListenerOnButton();
+    }
+    public void addListenerOnButton() {
+        bntChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                list2 = new ArrayList<>();
+                list2.add("Nono");
+                list2.add("Nini");
+                list2.add("Nana");
+                adapter.setListText(list2);
+            }
+        });
     }
 }
