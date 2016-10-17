bc27fe0fa861c120331a33e6bcb723dc842b0db1
josuearredondo <josue.arredondo@hotmail.fr>
2016-10-17 14:39:44 +0200

    Deleted Toast import

--- app/src/main/java/com/josue/flickr/MainActivity.java
+++ app/src/main/java/com/josue/flickr/MainActivity.java
@@ -10,7 +10,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
