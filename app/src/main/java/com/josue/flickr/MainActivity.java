958716756acc686a0adf8debf3ad79495f691aa8
josuearredondo <josue.arredondo@hotmail.fr>
2016-10-25 12:15:00 +0200

    Added FAM and button search

--- app/src/main/java/com/josue/flickr/MainActivity.java
+++ app/src/main/java/com/josue/flickr/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.IBinder;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -13,7 +12,11 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionButton;
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionMenu;
+import com.oguzdev.circularfloatingactionmenu.library.SubActionButton;
 
 import java.util.List;
 
@@ -38,6 +41,40 @@ public class MainActivity extends AppCompatActivity implements FlickrServiceList
         adapter = new AdapterFlickr (this);
         listView.setAdapter(adapter);
         addListenerOnButton();
+        // Create an icon to the FAB menu
+        ImageView icon = new ImageView(this);
+        icon.setImageResource(R.mipmap.ic_launcher);
+
+        FloatingActionButton actionButton = new FloatingActionButton.Builder(this)
+                .setContentView(icon)
+                .build();
+        SubActionButton.Builder itemBuilder = new SubActionButton.Builder(this);
+        // repeat many times:
+        ImageView itemIcon1 = new ImageView(this);
+        itemIcon1.setImageResource(R.mipmap.ic_launcher);
+
+        ImageView itemIcon2 = new ImageView(this);
+        itemIcon2.setImageResource(R.mipmap.ic_launcher);
+
+        ImageView itemIcon3 = new ImageView(this);
+        itemIcon3.setImageResource(R.mipmap.ic_launcher);
+
+        ImageView itemIcon4 = new ImageView(this);
+        itemIcon4.setImageResource(R.mipmap.ic_launcher);
+
+        SubActionButton button1 = itemBuilder.setContentView(itemIcon1).build();
+        SubActionButton button2 = itemBuilder.setContentView(itemIcon2).build();
+        SubActionButton button3 = itemBuilder.setContentView(itemIcon3).build();
+        SubActionButton button4 = itemBuilder.setContentView(itemIcon4).build();
+
+        //attach the sub buttons to the main button
+        FloatingActionMenu actionMenu = new FloatingActionMenu.Builder(this)
+                .addSubActionView(button1)
+                .addSubActionView(button2)
+                .addSubActionView(button3)
+                .addSubActionView(button4)
+                .attachTo(actionButton)
+                .build();
 
         editText.addTextChangedListener(new TextWatcher() {
             @Override
@@ -89,7 +126,7 @@ public class MainActivity extends AppCompatActivity implements FlickrServiceList
     };
 
     public void addListenerOnButton() {
-        fab.setOnClickListener(new View.OnClickListener() {
+        /*fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
                 if (!editText.getText().toString().equals("")) {
@@ -111,7 +148,7 @@ public class MainActivity extends AppCompatActivity implements FlickrServiceList
                     }
                 }
             }
-        });
+        });*/
     }
 
     @Override
